64da7aa3b6b3d6091a1cae4f0c88029c8c4f9174
ewel1991 <ewelina.beben.programista@gmail.com>
2024-10-06 08:28:59 +0200

    Dodanie bibliotek i wektorów

--- main.cpp
+++ main.cpp
@@ -1,1 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <vector>
+
+
+using namespace std;
+
+struct Uzytkownik
+{
+	int id;
+	string nazwa = "", haslo = "";
+};
+
+struct Osoba
+{
+    int id = 0;
+    string imie = "", nazwisko = "", numerTelefonu ="", email ="", adres ="";
+};
+
+
+
+int main()
+{
+    vector <Osoba> osoby;
+    vector <Uzytkownik> uzytkownicy;
+    Osoba osoba;
+    Uzytkownik uzytkownik;
+    char wybor;
+    int numerKolejnegoId = 0;
+
+
+
+
+}
 
