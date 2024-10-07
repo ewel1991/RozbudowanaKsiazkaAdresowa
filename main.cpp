ec553666a7abbfca0fad699b79b5c7a16c7e392d
ewel1991 <ewelina.beben.programista@gmail.com>
2024-10-07 20:34:32 +0200

    Dodanie uzytkownika

--- main.cpp
+++ main.cpp
@@ -40,12 +40,133 @@ char wczytajZnak()
     return znak;
 }
 
+string wczytajLinie()
+{
+    string wejscie ="";
+
+    cin.sync();
+    getline(cin, wejscie);
+    return wejscie;
+}
+
+int wczytajOsobyZPlikuUzytkownicy (vector <Uzytkownik> &uzytkownicy)
+{
+    Uzytkownik uzytkownik;
+    string linia = "", wyraz = "";
+    int nrKreski = 0, ostatniNumerUzytkownika = 0;
+
+    fstream plik;
+    plik.open("Uzytkownicy.txt", ios::in);
+
+    if (plik.good() == false)
+    {
+        return 0;
+    }
+    while (getline(plik, linia))
+    {
+
+        for (int i = 0; i < linia.length(); i++)
+        {
+
+            if (linia[i] != '|')
+            {
+                wyraz += linia [i];
+            }
+
+            else
+            {
+                nrKreski ++;
+
+
+                switch (nrKreski)
+                {
+                case 1:
+                    uzytkownik.id = stoi(wyraz);
+                    wyraz = "";
+                    break;
+                case 2:
+                    uzytkownik.nazwa = wyraz;
+                    wyraz = "";
+                    break;
+                case 3:
+                    uzytkownik.haslo = wyraz;
+                    wyraz = "";
+                    uzytkownicy.push_back(uzytkownik);
+                    nrKreski = 0;
+                    break;
+                }
+            }
+        }
+    }
+    plik.close();
+
+    return ostatniNumerUzytkownika = uzytkownik.id;
+}
 
 
 
 
 
 
+void dodajUzytkownikaDoPliku (Uzytkownik uzytkownik)
+{
+    fstream plik;
+    plik.open("Uzytkownicy.txt", ios::out | ios::app);
+
+    if (plik.good() == true)
+    {
+        plik << uzytkownik.id << "|";
+        plik << uzytkownik.nazwa << "|";
+        plik << uzytkownik.haslo << "|" << endl;
+    }
+    else
+    {
+        cout << "Nie udalo sie otworzyc pliku i zapisac do niego danych." << endl;
+        system("pause");
+    }
+    plik.close();
+}
+
+
+int rejestracja (vector <Uzytkownik> &uzytkownicy, int iloscUzytkownikow)
+{
+    string nazwa;
+	Uzytkownik uzytkownik;
+	cout <<"Podaj nazwe uzytkownika: ";
+	nazwa = wczytajLinie();
+
+	int i = 0;
+	while ( i < iloscUzytkownikow )
+	{
+	    for (Uzytkownik uzytkownik: uzytkownicy)
+        {
+            if (uzytkownik.nazwa == nazwa)
+    	{
+        	cout << "Taki uzytkownik istnieje. Wpisz inna nazwe uzytkownika: ";
+        	nazwa = wczytajLinie();
+        	i = 0;
+    	}
+    	else
+    	{
+        	i++;
+    	}
+        }
+
+	}
+	uzytkownik.nazwa = nazwa;
+
+	cout << "Podaj haslo:";
+	uzytkownik.haslo = wczytajLinie();
+
+	uzytkownik.id = iloscUzytkownikow+1;
+
+	uzytkownicy.push_back(uzytkownik);
+	cout << "Konto zalozone" << endl;
+	Sleep(1000);
+
+    dodajUzytkownikaDoPliku(uzytkownik);
+	return iloscUzytkownikow+1;
+}
 
 
 int main()
@@ -55,8 +176,11 @@ int main()
     Adresat adresat;
     Uzytkownik uzytkownik;
     char wybor;
-    int numerKolejnegoId = 0;
+    int numerKolejnegoId = 0, iloscUzytkownikow = 0;
     int idZalogowanegoUzytkownika = 0;
+
+    iloscUzytkownikow = wczytajOsobyZPlikuUzytkownicy(uzytkownicy);
+
 
     while(1)
     {
@@ -75,7 +199,7 @@ int main()
 
             if (wybor == '1')
             {
-                //iloscUzytkownikow = rejestracja(uzytkownicy, iloscUzytkownikow);
+                iloscUzytkownikow = rejestracja(uzytkownicy, iloscUzytkownikow);
             }
             else if (wybor == '2')
             {
